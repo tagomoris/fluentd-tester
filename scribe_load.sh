409a719dcac6bcbb90b610c42ddf54125b6d1a04
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-11-15 20:24:16 +0900

    wrote tests for benchmarking

--- scribe_load.sh
+++ scribe_load.sh
@@ -2,59 +2,86 @@
 
 target_host="rx30.standby"
 
-echo '1000 msg/s'
-python scribeline/misc/scribe_server_dummy.py -q 1463 &
-pid=$!
-ruby scribe_load.rb $target_host 1463 TESTING 100 50 20 10
+echo 'messages per seconds'
+echo ''
+date
+echo '1000 msgs/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 50 20 60
 sleep 1
-kill -HUP $pid
+echo ''
+date
+echo '4000 msgs/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 100 40 60
 sleep 1
+echo ''
+date
+echo '11000 msgs/s (10980)'
+ruby scribe_load.rb $target_host 1463 TESTING 100 183 60 60
+sleep 1
+echo ''
+date
+echo '16000 msgs/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 200 80 60
+sleep 1
+echo ''
+sleep 1800
+# 4.5 hours
 
-echo '2000 msg/s'
-python scribeline/misc/scribe_server_dummy.py -q 1463 &
-pid=$!
-ruby scribe_load.rb $target_host 1463 TESTING 100 50 40 10
+echo 'messages per rpc, for 16000 msgs/s'
+echo ''
+date
+echo '50 msgs/rpc * 320 rpc/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 50 320 60
 sleep 1
-kill -HUP $pid
+echo ''
+date
+echo '100 msgs/rpc * 160 rpc/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 100 160 60
 sleep 1
+echo ''
+date
+echo '200 msgs/rpc * 80 rpc/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 200 80 60
+sleep 1
+echo ''
+date
+echo '400 msgs/rpc * 40 rpc/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 400 40 60
+sleep 1
+echo ''
+date
+echo '800 msgs/rpc * 20 rpc/s'
+ruby scribe_load.rb $target_host 1463 TESTING 100 800 20 60
+sleep 1
+echo ''
+sleep 1800
+# 6.5 hours (+4.5=11)
 
-echo '4000 msg/s'
-python scribeline/misc/scribe_server_dummy.py -q 1463 &
-pid=$!
-ruby scribe_load.rb $target_host 1463 TESTING 100 100 40 10
+echo 'throuput(bytes/sec) for 10000 msgs/s'
+echo ''
+date
+echo '50 bytes/message'
+ruby scribe_load.rb $target_host 1463 TESTING 50 200 80 60
 sleep 1
-kill -HUP $pid
+echo ''
+date
+echo '100 bytes/message'
+ruby scribe_load.rb $target_host 1463 TESTING 100 200 80 60
 sleep 1
-
-echo '8000 msg/s'
-python scribeline/misc/scribe_server_dummy.py -q 1463 &
-pid=$!
-ruby scribe_load.rb $target_host 1463 TESTING 100 100 80 10
+echo ''
+date
+echo '200 bytes/message'
+ruby scribe_load.rb $target_host 1463 TESTING 200 200 80 60
 sleep 1
-kill -HUP $pid
+echo ''
+date
+echo '400 bytes/message'
+ruby scribe_load.rb $target_host 1463 TESTING 400 200 80 60
 sleep 1
-
-echo '16000 msg/s'
-python scribeline/misc/scribe_server_dummy.py -q 1463 &
-pid=$!
-ruby scribe_load.rb $target_host 1463 TESTING 100 200 80 10
+echo ''
+date
+echo '800 bytes/message'
+ruby scribe_load.rb $target_host 1463 TESTING 800 200 80 60
 sleep 1
-kill -HUP $pid
-sleep 1
-
-echo '32000 msg/s'
-python scribeline/misc/scribe_server_dummy.py -q 1463 &
-pid=$!
-ruby scribe_load.rb $target_host 1463 TESTING 100 400 80 10
-sleep 1
-kill -HUP $pid
-sleep 1
-
-echo '64000 msg/s'
-python scribeline/misc/scribe_server_dummy.py -q 1463 &
-pid=$!
-ruby scribe_load.rb $target_host 1463 TESTING 100 800 80 10
-sleep 1
-kill -HUP $pid
-sleep 1
-
+echo ''
+date
